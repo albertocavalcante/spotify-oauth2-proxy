dd00ff1c608c589b2b13ff88f7d7334555a9ea4e
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2024-01-28 13:33:34 -0300

    overwrite tailwind colors + add spotify color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./static/**/*.html"
   ],
   theme: {
+    // https://tailwindcss.com/docs/customizing-colors#using-the-default-colors
+    colors: {
+      "spotify": {
+        DEFAULT: "#1ED760",
+      },
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      green: colors.green,
+    },
     extend: {},
   },
   plugins: [],
